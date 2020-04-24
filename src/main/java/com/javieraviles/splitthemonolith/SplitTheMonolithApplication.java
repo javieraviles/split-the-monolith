499a335b62e75427312ffb93a47ce96aaa192033
Javier Aviles <avileslopez.javier@gmail.com>
2020-04-24 19:34:07 +0200

    Adding springboot seed from initializr

---
+++ src/main/java/com/javieraviles/splitthemonolith/SplitTheMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.javieraviles.splitthemonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitTheMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitTheMonolithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javieraviles/splitthemonolith/SplitTheMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javieraviles.splitthemonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitTheMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
