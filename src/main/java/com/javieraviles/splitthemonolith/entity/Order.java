5e1d2ac22d07c81c551ebf99cb7a82a26c30245f
Javier Aviles <avileslopez.javier@gmail.com>
2020-04-27 17:56:06 +0200

    Adding some bean validations to Order entity

--- src/main/java/com/javieraviles/splitthemonolith/entity/Order.java
+++ src/main/java/com/javieraviles/splitthemonolith/entity/Order.java
@@ -7,69 +7,75 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Entity(name = "orders")
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "customer")
-    private Customer customer;
+	@NotNull
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "customer")
+	private Customer customer;
 
-    private double totalCost;
+	@Positive
+	private double totalCost;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "product")
-    private Product product;
+	@NotNull
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "product")
+	private Product product;
 
-    private int productQuantity;
+	@Positive
+	private int productQuantity;
 
-    public Order() {
-    }
+	public Order() {
+	}
 
-    public Order(final Customer customer, final double totalCost, final Product product, final int productQuantity) {
-        this.customer = customer;
-        this.totalCost = totalCost;
-        this.product = product;
-        this.productQuantity = productQuantity;
-    }
+	public Order(final Customer customer, final double totalCost, final Product product, final int productQuantity) {
+		this.customer = customer;
+		this.totalCost = totalCost;
+		this.product = product;
+		this.productQuantity = productQuantity;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public Customer getCustomer() {
-        return customer;
-    }
+	public Customer getCustomer() {
+		return customer;
+	}
 
-    public void setCustomer(final Customer customer) {
-        this.customer = customer;
-    }
+	public void setCustomer(final Customer customer) {
+		this.customer = customer;
+	}
 
-    public double getTotalCost() {
-        return totalCost;
-    }
+	public double getTotalCost() {
+		return totalCost;
+	}
 
-    public void setTotalCost(final double totalCost) {
-        this.totalCost = totalCost;
-    }
+	public void setTotalCost(final double totalCost) {
+		this.totalCost = totalCost;
+	}
 
-    public Product getProduct() {
-        return product;
-    }
+	public Product getProduct() {
+		return product;
+	}
 
-    public void setProduct(final Product product) {
-        this.product = product;
-    }
+	public void setProduct(final Product product) {
+		this.product = product;
+	}
 
-    public int getProductQuantity() {
-        return productQuantity;
-    }
+	public int getProductQuantity() {
+		return productQuantity;
+	}
 
-    public void setProductQuantity(final int productQuantity) {
-        this.productQuantity = productQuantity;
-    }
+	public void setProductQuantity(final int productQuantity) {
+		this.productQuantity = productQuantity;
+	}
 }
